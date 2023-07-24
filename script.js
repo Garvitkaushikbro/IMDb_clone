127b986ba362047ee8088b8bd1ac1167229cc860
Garvitkaushikbro <garvitkaushik123@gmail.com>
2023-07-24 09:00:10 +0530

    IDMb_clone

--- script.js
+++ script.js
@@ -5,8 +5,61 @@ const slideTracker = document.querySelector(".slide-tracker");
 const slideContainer = document.querySelector(".slide-container");
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
+let position = 0;
+const mts = document.querySelectorAll(".movie-grid-item");
+const overlay = document.querySelector(".overlay");
+const showcase = document.querySelector(".showcase");
 
-function findMovies() {
+const removeModal = function () {
+  overlay.classList.add("hidden");
+  showcase.classList.add("hidden");
+  showcase.innerHTML = "";
+};
+
+overlay.addEventListener("click", removeModal);
+
+slideTracker.addEventListener("click", async function (e) {
+  if (e.target.tagName === "IMG") {
+    overlay.classList.remove("hidden");
+    showcase.classList.remove("hidden");
+    showcase.innerHTML = "";
+    const id = e.target.closest("div").dataset.id;
+    const URL = `https://omdbapi.com/?i=${id}&apikey=2e0ef59`;
+    // const URL = "http://www.omdbapi.com/?i=tt3896198&apikey=2e0ef59";
+    console.log(URL);
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    if (data.Response == "True") {
+      const details = data;
+      showcase.innerHTML = `
+      <div class = "movie-poster">
+          <img src = "${
+            details.Poster != "N/A" ? details.Poster : "image_not_found.png"
+          }" alt = "movie poster">
+      </div>
+      <div class = "movie-info">
+          <h3 class = "movie-title">${details.Title}</h3>
+          <ul class = "movie-misc-info">
+              <li class = "year">Year: ${details.Year}</li>
+              <li class = "rated">Ratings: ${details.Rated}</li>
+              <li class = "released">Released: ${details.Released}</li>
+          </ul>
+          <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+          <p class = "actors"><b>Actors: </b>${details.Actors}</p>
+          <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
+          <p class = "language"><b>Language:</b> ${details.Language}</p>
+          <p class = "awards"><b><i class = "fas fa-award"></i></b> ${
+            details.Awards
+          }</p>
+      </div>
+      `;
+    }
+  }
+});
+
+function findMovies(event) {
+  if (event.key !== "Enter") return;
+  position = 0;
   let searchTerm = movieSearchBox.value.trim();
   slideTracker.innerHTML = "";
   if (searchTerm.length > 0) {
@@ -19,64 +72,62 @@ function findMovies() {
 
 // load movies from API
 async function loadMovies(searchTerm) {
-  const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=74986d14`;
+  const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=2e0ef59`;
+  console.log(URL);
   const res = await fetch(`${URL}`);
   const data = await res.json();
   if (data.Response == "True") {
-    // displayMovieList(data.Search);
     const totalResults = parseInt(data.totalResults);
     const resultsPerPage = data.Search ? data.Search.length : 0;
 
     // Calculate the total number of pages
     const totalPages =
       resultsPerPage === 0 ? 0 : Math.ceil(totalResults / resultsPerPage);
-
     console.log(totalPages);
-    displayMoviePageGrid(totalPages, searchTerm);
-  }
-}
-
-async function displayMoviePageGrid(totalPages, searchTerm) {
-  slideTracker.innerHTML = "";
-  for (let i = 1; i <= totalPages; i++) {
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=${i}&apikey=74986d14`;
-    const res = await fetch(`${URL}`);
-    const data = await res.json();
-
-    let movieGrid = document.createElement("div");
-    movieGrid.classList.add("movie-grid");
-    movieGrid.dataset.id = i; // setting page number of the grid
-
-    const movies = data.Search;
-    // setting grid items in moviegrid
-    for (let idx = 0; idx < data.Search.length; idx++) {
-      let movieGridItem = document.createElement("div");
-      movieGridItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
-      movieGridItem.classList.add("movie-grid-Item");
-      let title =
-        movies[idx].Title.length > 24
-          ? movies[idx].Title.slice(0, 21) + "..."
-          : movies[idx].Title;
-      let moviePoster;
-      if (movies[idx].Poster != "N/A") moviePoster = movies[idx].Poster;
-      else moviePoster = "image_not_found.png";
-      movieGridItem.innerHTML = `
-        <div class = "movie-grid-item-thumbnail">
-            <img src = "${moviePoster}">
-        </div>
-        <div class = "movie-grid-item-info">
-            <h3>${title}</h3>
-            <p>${movies[idx].Year}</p>
-        </div>
-        `;
-
-      movieGrid.appendChild(movieGridItem);
+    // Fetch and add movie data for each page separately
+    for (let i = 1; i <= totalPages; i++) {
+      await addMoviesToSlider(searchTerm, i);
     }
-    slideTracker.appendChild(movieGrid);
   }
 }
 
-let position = 0;
+// Function to fetch and add movie data for a specific page
+async function addMoviesToSlider(searchTerm, page) {
+  const URL = `https://omdbapi.com/?s=${searchTerm}&page=${page}&apikey=2e0ef59`;
+  const res = await fetch(`${URL}`);
+  const data = await res.json();
+
+  let movieGrid = document.createElement("div");
+  movieGrid.classList.add("movie-grid");
+  movieGrid.dataset.id = page; // setting page number of the grid
+  const movies = [...data.Search];
+
+  // setting grid items in moviegrid
+  for (let idx = 0; idx < data.Search.length; idx++) {
+    let movieGridItem = document.createElement("div");
+    movieGridItem.dataset.id = movies[idx].imdbID; // setting movie id in data-id
+    movieGridItem.classList.add("movie-grid-Item");
+    let title =
+      movies[idx].Title.length > 24
+        ? movies[idx].Title.slice(0, 21) + "..."
+        : movies[idx].Title;
+    let moviePoster;
+    if (movies[idx].Poster != "N/A") moviePoster = movies[idx].Poster;
+    else moviePoster = "image_not_found.png";
+    movieGridItem.innerHTML = `
+      <div class="movie-grid-item-thumbnail" data-id=${movies[idx].imdbID}>
+          <img src="${moviePoster}">
+      </div>
+      <div class="movie-grid-item-info">
+          <h3>${title}</h3>
+          <p>${movies[idx].Year}</p>
+      </div>
+    `;
+
+    movieGrid.appendChild(movieGridItem);
+  }
+  slideTracker.appendChild(movieGrid);
+}
 
 function slide() {
   slideTracker.style.transform = `translateX(${position}px)`;
@@ -92,34 +143,87 @@ rightArrow.addEventListener("click", () => {
   slide();
 });
 
-function displayMovieList(movies) {
-  searchList.innerHTML = "";
-  for (let idx = 0; idx < movies.length; idx++) {
-    let movieListItem = document.createElement("div");
-    movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
-    movieListItem.classList.add("search-list-item");
-    if (movies[idx].Poster != "N/A") moviePoster = movies[idx].Poster;
-    else moviePoster = "image_not_found.png";
-
-    movieListItem.innerHTML = `
-        <div class = "search-item-thumbnail">
-            <img src = "${moviePoster}">
-        </div>
-        <div class = "search-item-info">
-            <h3>${movies[idx].Title}</h3>
-            <p>${movies[idx].Year}</p>
-        </div>
-        `;
-    searchList.appendChild(movieListItem);
-  }
-  loadMovieDetails();
-}
-
 window.addEventListener("click", (event) => {
   if (event.target.className != "form-control") {
     searchList.classList.add("hide-search-list");
   }
 });
+
+// // load movies from API
+// async function loadMovies(searchTerm) {
+//   const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=74986d14`;
+//   const res = await fetch(`${URL}`);
+//   const data = await res.json();
+//   if (data.Response == "True") {
+//     // displayMovieList(data.Search);
+//     const totalResults = parseInt(data.totalResults);
+//     const resultsPerPage = data.Search ? data.Search.length : 0;
+
+//     // Calculate the total number of pages
+//     const totalPages =
+//       resultsPerPage === 0 ? 0 : Math.ceil(totalResults / resultsPerPage);
+
+//     for (let i = 1; i <= totalPages; i++) {
+//       const URL = `https://omdbapi.com/?s=${searchTerm}&page=${i}&apikey=74986d14`;
+//       const res = await fetch(`${URL}`);
+//       const data = await res.json();
+
+//       let movieGrid = document.createElement("div");
+//       movieGrid.classList.add("movie-grid");
+//       movieGrid.dataset.id = i; // setting page number of the grid
+//       const movies = [...data.Search];
+
+//       // setting grid items in moviegrid
+//       for (let idx = 0; idx < data.Search.length; idx++) {
+//         let movieGridItem = document.createElement("div");
+//         movieGridItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+//         movieGridItem.classList.add("movie-grid-Item");
+//         let title =
+//           movies[idx].Title.length > 24
+//             ? movies[idx].Title.slice(0, 21) + "..."
+//             : movies[idx].Title;
+//         let moviePoster;
+//         if (movies[idx].Poster != "N/A") moviePoster = movies[idx].Poster;
+//         else moviePoster = "image_not_found.png";
+//         movieGridItem.innerHTML = `
+//           <div class = "movie-grid-item-thumbnail">
+//               <img src = "${moviePoster}">
+//           </div>
+//           <div class = "movie-grid-item-info">
+//               <h3>${title}</h3>
+//               <p>${movies[idx].Year}</p>
+//           </div>
+//           `;
+
+//         movieGrid.appendChild(movieGridItem);
+//       }
+//       slideTracker.appendChild(movieGrid);
+//     }
+//   }
+// }
+
+// function displayMovieList(movies) {
+//   searchList.innerHTML = "";
+//   for (let idx = 0; idx < movies.length; idx++) {
+//     let movieListItem = document.createElement("div");
+//     movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+//     movieListItem.classList.add("search-list-item");
+//     if (movies[idx].Poster != "N/A") moviePoster = movies[idx].Poster;
+//     else moviePoster = "image_not_found.png";
+
+//     movieListItem.innerHTML = `
+//         <div class = "search-item-thumbnail">
+//             <img src = "${moviePoster}">
+//         </div>
+//         <div class = "search-item-info">
+//             <h3>${movies[idx].Title}</h3>
+//             <p>${movies[idx].Year}</p>
+//         </div>
+//         `;
+//     searchList.appendChild(movieListItem);
+//   }
+//   loadMovieDetails();
+// }
 
 // function loadMovieDetails() {
 //   const searchListMovies = searchList.querySelectorAll(".search-list-item");
@@ -139,27 +243,27 @@ window.addEventListener("click", (event) => {
 // }
 
 // function displayMovieDetails(details) {
-//   resultGrid.innerHTML = `
-//     <div class = "movie-poster">
-//         <img src = "${
-//           details.Poster != "N/A" ? details.Poster : "image_not_found.png"
-//         }" alt = "movie poster">
-//     </div>
-//     <div class = "movie-info">
-//         <h3 class = "movie-title">${details.Title}</h3>
-//         <ul class = "movie-misc-info">
-//             <li class = "year">Year: ${details.Year}</li>
-//             <li class = "rated">Ratings: ${details.Rated}</li>
-//             <li class = "released">Released: ${details.Released}</li>
-//         </ul>
-//         <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
-//         <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
-//         <p class = "actors"><b>Actors: </b>${details.Actors}</p>
-//         <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
-//         <p class = "language"><b>Language:</b> ${details.Language}</p>
-//         <p class = "awards"><b><i class = "fas fa-award"></i></b> ${
-//           details.Awards
-//         }</p>
-//     </div>
-//     `;
+// resultGrid.innerHTML = `
+//   <div class = "movie-poster">
+//       <img src = "${
+//         details.Poster != "N/A" ? details.Poster : "image_not_found.png"
+//       }" alt = "movie poster">
+//   </div>
+//   <div class = "movie-info">
+//       <h3 class = "movie-title">${details.Title}</h3>
+//       <ul class = "movie-misc-info">
+//           <li class = "year">Year: ${details.Year}</li>
+//           <li class = "rated">Ratings: ${details.Rated}</li>
+//           <li class = "released">Released: ${details.Released}</li>
+//       </ul>
+//       <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+//       <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
+//       <p class = "actors"><b>Actors: </b>${details.Actors}</p>
+//       <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
+//       <p class = "language"><b>Language:</b> ${details.Language}</p>
+//       <p class = "awards"><b><i class = "fas fa-award"></i></b> ${
+//         details.Awards
+//       }</p>
+//   </div>
+//   `;
 // }
